a5f28832fd8edfacbe77dafbe61aec959c00b700
chuy-zip <andreschuy03@gmail.com>
2024-08-10 17:28:58 -0600

    changed textures

--- src/caster.rs
+++ src/caster.rs
@@ -10,8 +10,8 @@ pub struct Intersect {
 
 pub fn load_textures() -> (DynamicImage, DynamicImage, DynamicImage, DynamicImage) {
     let texture_plus = image::open("./src/img/BRICK_1A.PNG").unwrap();
-    let texture_minus = image::open("./src/img/BRICK_1A.PNG").unwrap();
-    let texture_pipe = image::open("./src/img/BRICK_1A.PNG").unwrap();
+    let texture_minus = image::open("./src/img/BRICK_3B.PNG").unwrap();
+    let texture_pipe = image::open("./src/img/BRICK_6D.PNG").unwrap();
     let texture_g = image::open("./src/img/BRICK_1A.PNG").unwrap();
     (texture_plus, texture_minus, texture_pipe, texture_g)
 }
